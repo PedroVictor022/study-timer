a85f50391bbcb4a7f5cddb933044148950b6b4a6
PedroVictor022 <pedro20002victor@gmail.com>
2023-06-27 10:09:52 -0300

    redefine type Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button/Button'
+import { defaultTheme } from './styles/theme/default'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary">Primary</Button>
-    </>
+    </ThemeProvider>
   )
 }
 
