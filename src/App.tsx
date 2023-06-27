204561bce49ccd2fee0d18bc4b13d46a60b5ce73
PedroVictor022 <pedro20002victor@gmail.com>
2023-06-27 09:56:06 -0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from './components/Button/Button'
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+    </>
+  )
+}
+
+export default App
